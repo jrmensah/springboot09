0e67cd61776e20ec28a64b8e580fc2b6fc2c8bb7
jrmensah <jmensah4@student.umuc.edu>
2017-08-22 09:53:36 -0400

    Added jobform and list HTML files

--- src/main/java/me/jerilynmensah/springboot09/HomeController.java
+++ src/main/java/me/jerilynmensah/springboot09/HomeController.java
@@ -4,8 +4,12 @@ package me.jerilynmensah.springboot09;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 public class HomeController {
@@ -24,5 +28,12 @@ public class HomeController {
         model.addAttribute("job", new Job());
         return "jobform";
     }
-    
+    @PostMapping("/process")
+    public String processForm(@Valid Job job, BindingResult result){
+        if(result.hasErrors()){
+            return "jobform";
+        }
+        jobRepository.save(job);
+        return "redirect:/";
+    }
 }
